51927bcbee9f6c7c4114962699d5e54a950b9358
lfnsyhd <alfansyahadahu@gmail.com>
2025-01-21 18:31:55 +0700

    Implement widget total data in Admin Dashboard

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -4,36 +4,55 @@ import UserLayout from '../../layouts/UserLayout';
 import BoxWidget from '../../components/BoxWidget';
 import { FaRegUser } from 'react-icons/fa';
 import { LuHandCoins, LuHandshake } from 'react-icons/lu';
+import { useEffect, useState } from 'react';
+import useGetAllBooks from '../../hooks/useGetAllBooks';
+import useGetAllUsers from '../../hooks/useGetAllUsers';
+import useGetAllBorrowed from '../../hooks/useGetAllBorrowed';
 
 const AdminDashboard = () => {
   const { user } = useUser();
+  const [totalBook, setTotalBook] = useState<number>(0);
+  const [totalUser, setTotalUser] = useState<number>(0);
+  const [totalLending, setTotalLending] = useState<number>(0);
+
+  const { data: booksData } = useGetAllBooks(null);
+  const { data: usersData } = useGetAllUsers();
+  const { data: borrowedData } = useGetAllBorrowed(null);
+
+  const updateTotals = () => {
+    setTotalBook(booksData?.length || 0);
+    setTotalUser(usersData?.user?.length || 0);
+    setTotalLending(borrowedData?.length || 0);
+  };
+
+  useEffect(() => {
+    if (booksData && usersData && borrowedData) {
+      updateTotals();
+    }
+  }, [booksData, usersData, borrowedData]);
+
   return (
     <>
       <UserLayout>
         <div>
-          <h1 className='text-2xl font-bold mb-2 text-white'>Hi, { user?.name?.split(' ')[0] } !</h1>
+          <h1 className='text-2xl font-bold mb-2 text-white'>Hi, {user?.name?.split(' ')[0]} !</h1>
           <span className='text-sm text-gray-300'>Here is our summary for you</span>
 
-          <div className='mt-4 grid lg:grid-cols-3 xl:grid-cols-4 grid-cols-2 gap-3'>
+          <div className='mt-4 grid lg:grid-cols-3 grid-cols-2 gap-3'>
             <BoxWidget
               icon={<GrBook size={40} color='#FFF' />}
               title='Total Book'
-              amount={3}
+              amount={totalBook}
             />
             <BoxWidget
               icon={<FaRegUser size={40} color='#FFF' />}
               title='Total User'
-              amount={3}
+              amount={totalUser}
             />
             <BoxWidget
               icon={<LuHandshake size={40} color='#FFF' />}
               title='Total Lending'
-              amount={3}
-            />
-            <BoxWidget
-              icon={<LuHandCoins size={40} color='#FFF' />}
-              title='Lending Active'
-              amount={3}
+              amount={totalLending}
             />
           </div>
         </div>
