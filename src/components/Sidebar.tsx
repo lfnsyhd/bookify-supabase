11e05d13196fcfaa9453497809fe432b9ead26fa
lfnsyhd <alfansyahadahu@gmail.com>
2025-01-23 23:51:59 +0700

    Adjust padding on sidebar mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,11 @@ const Sidebar = ({children}: {children: React.ReactNode}) => {
   const [show, setShow] = useState<boolean>(true);
 
   return (
-    <div className='bg-[#121212] w-full md:w-[300px] h-fit max-h-[calc(100vh-20px)] rounded-xl text-white overflow-y-auto pt-5 pb-6 pl-7 pr-4 md:pr-none'>
+    <div className='bg-[#121212] w-full md:w-[300px] h-fit max-h-[calc(100vh-20px)] rounded-xl text-white overflow-y-auto py-5 px-7 md:pr-none mt-2.5 md:mt-0'>
       <div className="flex justify-between items-center">
         <span className="text-[11px] text-gray-400 font-semibold uppercase tracking-[4px]">Menu</span>
         <div
-          className={`cursor-pointer transform transition-transform duration-200 ease-in-out ${!show && 'rotate-[-90deg]'}`}
+          className={`relative right-[-5px] md:right-0 cursor-pointer transform transition-transform duration-200 ease-in-out ${!show && 'rotate-[-90deg]'}`}
           onClick={() => setShow(!show)}
         >
           <MdKeyboardArrowDown size={25} color="#a1a1a1" />
